37218b55f62ca101d97be7fc639bb275eaccb7f6
Martin <martin1.bugar@gmail.com>
2020-09-03 10:04:27 +0200

    uvod

---
+++ src/com/prometheus/HelloWorld.java
@@ -0,0 +1,27 @@
+package com.prometheus;
+
+public class HelloWorld {
+    public void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+
+    public void printHelloWorld2 (HelloWorldFunctions helloWorldFunctions){
+        helloWorldFunctions.sayHello();
+    }
+
+    public static void main(String[] args) {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.printHelloWorld();
+
+        helloWorld.printHelloWorld2(helloWorld.new HelloWorldC());
+
+    }
+
+    class HelloWorldC implements HelloWorldFunctions{
+
+        @Override
+        public void sayHello() {
+            System.out.println("Hello World");
+        }
+    }
+}
